7886271db06cf38f42cbd4cfe5849bf17ca77f48
Brad Roberts <brad@bradroberts.net>
2018-02-09 13:37:11 -0500

    Added Ministry Platform config

---
+++ config/ministryplatform.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'domain'   => env('MINISTRY_PLATFORM_DOMAIN'),
+    'username' => env('MINISTRY_PLATFORM_USERNAME'),
+    'password' => env('MINISTRY_PLATFORM_PASSWORD'),
+
+];
