bedbb4d5f1bf5eb205199f27ef4ffef6d58f190d
Brad Roberts <brad@bradroberts.net>
2018-02-09 22:58:57 -0500

    Updated config

--- config/ministryplatform.php
+++ config/ministryplatform.php
@@ -2,8 +2,9 @@
 
 return [
 
-    'domain'   => env('MINISTRY_PLATFORM_DOMAIN'),
-    'username' => env('MINISTRY_PLATFORM_USERNAME'),
-    'password' => env('MINISTRY_PLATFORM_PASSWORD'),
+    'domain'              => env('MINISTRY_PLATFORM_DOMAIN'),
+    'username'            => env('MINISTRY_PLATFORM_USERNAME'),
+    'password'            => env('MINISTRY_PLATFORM_PASSWORD'),
+    'small_group_type_id' => env('MINISTRY_PLATFORM_SMALL_GROUP_TYPE_ID', 1),
 
 ];
