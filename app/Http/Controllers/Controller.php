513f9387aa30422c29d1bc7213d0df758e23a841
Brad Roberts <brad@bradroberts.net>
2018-02-09 22:59:41 -0500

    Endpoint for groups data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Models\Group;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+class Controller extends BaseController {
+
+    public function groups() {
+
+        $groups = Group::query()->with(['campus', 'focus', 'lifeStage'])->get();
+
+        $data = [];
+
+        /** @var Group $group */
+        foreach ($groups as $group) {
+            $data[] = [
+                'id'                 => $group->id,
+                'campus'             => $group->campus ? ['slug' => $group->campus->id, 'title' => $group->campus->name,] : null,
+                'focus'              => $group->focus ? ['slug' => $group->focus->id, 'title' => $group->focus->name,] : null,
+                'life_stage'         => $group->lifeStage ? ['slug' => $group->lifeStage->id, 'title' => $group->lifeStage->name,] : null,
+                'name'               => $group->name,
+                'description'        => $group->description,
+                'day_of_week'        => $group->day_of_week,
+                'frequency'          => $group->frequency,
+                'contact_first_name' => $group->contact_first_name,
+                'contact_last_name'  => $group->contact_last_name,
+                'street'             => $group->street,
+                'city'               => $group->city,
+                'state'              => $group->state,
+                'zip'                => $group->zip,
+                'latitude'           => $group->latitude,
+                'longitude'          => $group->longitude,
+                'first_meeting_at'   => $group->first_meeting_at,
+            ];
+        }
+
+        return response()->json($data);
+
+    }
+
 }
