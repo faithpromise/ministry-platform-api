ddcfac98f900ae9e72098d6d6f37336dea615dc7
Brad Roberts <brad@bradroberts.net>
2018-02-17 10:31:01 -0500

    After importing groups, ping the Craft CMS FeedMe plugin's URL to import groups into CMS.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel {
      * @var array
      */
     protected $commands = [
-        ImportGroups::class
+        ImportGroups::class,
     ];
 
     /**
@@ -24,7 +24,10 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule) {
         $schedule->command('groups:import')->twiceDaily(9, 16)->thenPing(config('ministryplatform.heartbeat_import_groups'));
-        $schedule->command('groups:geocode')->everyFiveMinutes()->thenPing(config('ministryplatform.heartbeat_import_groups'));
+        $schedule->command('groups:geocode')
+            ->everyFiveMinutes()
+            ->thenPing(config('ministryplatform.heartbeat_import_groups'))
+            ->thenPing(config('ministryplatform.cms_import_groups_url'));
     }
 
     /**
--- config/ministryplatform.php
+++ config/ministryplatform.php
@@ -8,5 +8,6 @@ return [
     'small_group_type_id' => env('MINISTRY_PLATFORM_SMALL_GROUP_TYPE_ID', 1),
 
     'heartbeat_import_groups' => env('HEARTBEAT_IMPORT_GROUPS', 'http://127.0.0.1'),
+    'cms_import_groups_url'   => env('CMS_IMPORT_GROUPS_URL', 'http://127.0.0.1'),
 
 ];
