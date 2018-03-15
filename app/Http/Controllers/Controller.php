9bc421b88e92040d324e1fb3c7ad18aac51fabec
Brad Roberts <brad@bradroberts.net>
2018-03-15 15:12:59 -0400

    Refactored to use transformers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,42 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use App\Transformers\GroupTransformer;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController {
 
     public function groups() {
 
-        $groups = Group::query()->with(['campus', 'focus', 'lifeStage'])->get();
+        $groups = Group::query()->with(['campus', 'focus', 'lifeStage', 'leaders'])->get();
 
-        $data = [];
+        $result = fractal($groups, new GroupTransformer);
+        $result->parseIncludes('leaders');
 
-        /** @var Group $group */
-        foreach ($groups as $group) {
-            $data[] = [
-                'id'                 => $group->id,
-                'campus'             => $group->campus ? ['slug' => $group->campus->remote_slug, 'title' => $group->campus->name,] : null,
-                'focus'              => $group->focus ? ['slug' => $group->focus->id, 'title' => $group->focus->name,] : null,
-                'life_stage'         => $group->lifeStage ? ['slug' => $group->lifeStage->id, 'title' => $group->lifeStage->name,] : null,
-                'name'               => $group->name,
-                'subtitle'           => $group->subtitle,
-                'description'        => $group->description,
-                'day_of_week'        => $group->day_of_week,
-                'start_time'         => $group->start_time,
-                'frequency'          => $group->frequency,
-                'contact_first_name' => $group->contact_first_name,
-                'contact_last_name'  => $group->contact_last_name,
-                'street'             => $group->street,
-                'city'               => $group->city,
-                'state'              => $group->state,
-                'zip'                => $group->zip,
-                'latitude'           => $group->latitude,
-                'longitude'          => $group->longitude,
-                'first_meeting_at'   => $group->first_meeting_at->format('Y-m-d H:i:s'),
-            ];
-        }
-
-        return response()->json($data);
+        return $result->respond();
 
     }
 
